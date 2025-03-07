58667d6d207c7193978d7bf45201800e5fa50e28
Enricco Gemha <enriccogemha@gmail.com>
2025-03-07 16:26:13 -0500

    fix: `get_metrics` and `get_metrics_ranges`

--- src/application.py
+++ src/application.py
@@ -23,19 +23,25 @@ def get_metrics(data):
   """
   Get repository data from the JSON file.
   """
-  return [{
-    "unique_contributors_count": repo_data["unique_contributors_count"],
-    "median_contributions_per_contributor": repo_data["median_contributions_per_contributor"],
-    "mean_contributions_per_contributor": repo_data["mean_contributions_per_contributor"],
-    "average_weekly_commits": repo_data["average_weekly_commits"],
-    "commit_consistency": repo_data["commit_consistency"],
-  } for repo_data in data]
+  result = []
+  for repo_data in data:
+    if not repo_data["unique_contributors_count"]:
+      print(f"Error: 'unique_contributors_count' not found in repository {repo_data['full_name']}")
+      continue
+    result.append({
+      "unique_contributors_count": repo_data["unique_contributors_count"],
+      "median_contributions_per_contributor": repo_data["median_contributions_per_contributor"],
+      "mean_contributions_per_contributor": repo_data["mean_contributions_per_contributor"],
+      "average_weekly_commits": repo_data["average_weekly_commits"],
+      "commit_consistency": repo_data["commit_consistency"],
+    })
+  return result
 
-def get_metrics_ranges(data):
+
+def get_metrics_ranges(metrics):
   """
   Get the range of repository data.
   """
-  metrics = get_metrics(data)
   return {
     "unique_contributors_count": (min([repo["unique_contributors_count"] for repo in metrics]), max([repo["unique_contributors_count"] for repo in metrics])),
     "median_contributions_per_contributor": (min([repo["median_contributions_per_contributor"] for repo in metrics]), max([repo["median_contributions_per_contributor"] for repo in metrics])),
@@ -43,3 +49,8 @@ def get_metrics_ranges(data):
     "average_weekly_commits": (min([repo["average_weekly_commits"] for repo in metrics]), max([repo["average_weekly_commits"] for repo in metrics])),
     "commit_consistency": (min([repo["commit_consistency"] for repo in metrics]), max([repo["commit_consistency"] for repo in metrics])),
   }
+
+# test the functions
+data = load_repo_data(FILE_LOCATION)
+metrics = get_metrics(data)
+metrics_ranges = get_metrics_ranges(metrics)
