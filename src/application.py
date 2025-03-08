2de429abd90506ffb032cc1cd86f019139d3573d
Enricco Gemha <enriccogemha@gmail.com>
2025-03-07 23:04:49 -0500

    refactor: Remove unused metrics functions and streamline data loading in application

--- src/application.py
+++ src/application.py
@@ -19,35 +19,6 @@ def load_repo_data(file_path):
     print(f"Error: File '{file_path}' contains invalid JSON")
     return
 
-def get_metrics(data):
-  """
-  Get repository data from the JSON file.
-  """
-  result = []
-  for repo_data in data:
-    if not repo_data["unique_contributors_count"]:
-      print(f"Error: 'unique_contributors_count' not found in repository {repo_data['full_name']}")
-      continue
-    result.append({
-      "unique_contributors_count": repo_data["unique_contributors_count"],
-      "median_contributions_per_contributor": repo_data["median_contributions_per_contributor"],
-      "mean_contributions_per_contributor": repo_data["mean_contributions_per_contributor"],
-      "average_weekly_commits": repo_data["average_weekly_commits"],
-      "commit_consistency": repo_data["commit_consistency"],
-    })
-  return result
-
-def get_metrics_ranges(metrics):
-  """
-  Get the range of repository data.
-  """
-  return {
-    "unique_contributors_count": (min([repo["unique_contributors_count"] for repo in metrics]), max([repo["unique_contributors_count"] for repo in metrics])),
-    "median_contributions_per_contributor": (min([repo["median_contributions_per_contributor"] for repo in metrics]), max([repo["median_contributions_per_contributor"] for repo in metrics])),
-    "mean_contributions_per_contributor": (min([repo["mean_contributions_per_contributor"] for repo in metrics]), max([repo["mean_contributions_per_contributor"] for repo in metrics])),
-    "average_weekly_commits": (min([repo["average_weekly_commits"] for repo in metrics]), max([repo["average_weekly_commits"] for repo in metrics])),
-    "commit_consistency": (min([repo["commit_consistency"] for repo in metrics]), max([repo["commit_consistency"] for repo in metrics])),
-  }
 
 def get_min_max_metrics():
   """
--- src/main.py
+++ src/main.py
@@ -2,10 +2,6 @@ from application import FILE_LOCATION, load_repo_data, get_metrics, get_metrics_
 from algorithm import particle_swarm_optimization
 
 
-data = load_repo_data(FILE_LOCATION)
-metrics = get_metrics(data)
-metrics_ranges = get_metrics_ranges(metrics)
-
 best_pos, best_val = particle_swarm_optimization(dim=9, bounds=get_min_max_metrics(), reward_function=reward_function)
 print("Best Position:", best_pos)
 print("Best Value:", best_val)
